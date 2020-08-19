a1b02b96a8624e328055de591010444ca82e22c3
Jeremy McWilliams <j2macwilliams@gmail.com>
2020-08-19 09:29:19 -0700

    update cpu run logic

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,6 +2,7 @@
 
 import sys
 
+
 class CPU:
     """Main CPU class."""
 
@@ -13,7 +14,6 @@ class CPU:
         self.pc = 0
         self.mar = 0
         self.running = True
-        
 
     def load(self, filename):
         """Load a program into memory."""
@@ -25,7 +25,7 @@ class CPU:
                     n = comment_split[0].strip()
                     if n == '':
                         continue
-                    val = int(n ,2)
+                    val = int(n, 2)
                     # store the val in ram
                     self.ram[address] = val
 
@@ -35,19 +35,22 @@ class CPU:
         except FileNotFoundError:
             print(f"{sys.argv[0]}: {filename} not found")
             sys.exit(2)
-        
+
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
+
         elif op == "SUB":
             self.reg[reg_a] -= self.reg[reg_b]
+
         elif op == "MUL":
             self.reg[reg_a] *= self.reg[reg_b]
-        elif op == "MUL":
-            self.reg[reg_a] *= self.reg[reg_b]
-        
+
+        elif op == "DIV":
+            self.reg[reg_a] /= self.reg[reg_b]
+
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -59,8 +62,8 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -81,63 +84,64 @@ class CPU:
         SUB = 161
         MUL = 162
         DIV = 163
-        
 
         isRunning = True
 
-        
         while isRunning:
             # Fetch
             cmd = self.ram_read(self.pc)
             # Decode
-            
-            # operand a
-            operand_a = self.pc + 1
-            # operand b
-            operand_b = self.pc + 2
-            
+
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+
             # op_size = 1
-            op_size = (cmd >> 6) + 1
+            op_size = ((cmd >> 6) & 0b11) + 1
             # alu_operations
-            # alu_op = (cmd >> 5) 
+            # alu_op = (cmd >> 5)
 
             # loops thru if/elif checks and returns something
-            if cmd == LDI: #HLT
-                num_to_save = self.ram[operand_b] # 300
-                reg_index = self.ram[operand_a]
-                self.reg[reg_index] = num_to_save
-                
+            if cmd == LDI:  # HLT
+                self.reg[operand_a] = operand_b
+                # num_to_save = self.ram[operand_b]  # 300
+                # reg_index = self.ram[operand_a]
+                # self.reg[reg_index] = num_to_save
+
             elif cmd == PRN:
-                index_of_reg = self.ram[operand_a]
-                num_at_reg = self.reg[index_of_reg]
-                print(num_at_reg)
+                print(self.reg[operand_a])
+                # index_of_reg = self.ram[operand_a]
+                # num_at_reg = self.reg[index_of_reg]
+                # print(num_at_reg)
             elif cmd == HLT:
                 isRunning = False
             elif cmd == ADD:
-                a_reg_index = self.ram[operand_a]
-                b_reg_index = self.ram[operand_b]
-                self.reg[a_reg_index] += self.reg[b_reg_index]
+                self.alu('ADD', operand_a, operand_b)
+                # a_reg_index = self.ram[operand_a]
+                # b_reg_index = self.ram[operand_b]
+                # self.reg[a_reg_index] += self.reg[b_reg_index]
             elif cmd == SUB:
-                a_reg_index = self.ram[operand_a]
-                b_reg_index = self.ram[operand_b]
-                self.reg[a_reg_index] -= self.reg[b_reg_index]
+                self.alu('SUB', operand_a, operand_b)
+                # a_reg_index = self.ram[operand_a]
+                # b_reg_index = self.ram[operand_b]
+                # self.reg[a_reg_index] -= self.reg[b_reg_index]
             elif cmd == MUL:
-                a_reg_index = self.ram[operand_a]
-                b_reg_index = self.ram[operand_b]
-                self.reg[a_reg_index] *= self.reg[b_reg_index]
+                self.alu('MUL', operand_a, operand_b)
+                # a_reg_index = self.ram[operand_a]
+                # b_reg_index = self.ram[operand_b]
+                # self.reg[a_reg_index] *= self.reg[b_reg_index]
             elif cmd == DIV:
-                a_reg_index = self.ram[operand_a]
-                b_reg_index = self.ram[operand_b]
-                self.reg[a_reg_index] /= self.reg[b_reg_index]
+                self.alu('DIV', operand_a, operand_b)
+                # a_reg_index = self.ram[operand_a]
+                # b_reg_index = self.ram[operand_b]
+                # self.reg[a_reg_index] /= self.reg[b_reg_index]
 
             self.pc += op_size
-            
 
     def ram_read(self, mar):
         """Read from the Ram"""
         # MAR
         return self.ram[mar]
-        
+
     def ram_write(self, mar, mdr):
         """Read / write from/to the Ram"""
         # MDR
